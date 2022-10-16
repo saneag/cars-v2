5cca4a351d7b88026601c4b4681f950b66666e02
saneag <garstea2002@mail.ru>
2022-10-16 13:30:09 +0300

    finished unit tests

--- server/tests/appTest.js
+++ server/tests/appTest.js
@@ -1,14 +1,37 @@
 
-import { expect } from "chai";
+import { assert, expect } from "chai";
 import request from 'request'
+import UserModel from '../models/User.js'
 
-describe('App', () => {
-    describe('Database connection', () => {
-        it('should connect to database', (done) => {
-            request('http://localhost:5000/cars', (err, response, body) => {
-                expect(response.statusCode).to.equal(200)
-                done()
-            })
-        });
+describe('Get /cars', () => {
+    it('should return all cars', (done) => {
+        request.get('http://localhost:5000/cars', (err, res, body) => {
+            assert.equal(res.statusCode, 200)
+            done()
+        })
+    })
+})
+
+describe('Get /cars/:id', () => {
+    it('should return one car by id', (done) => {
+        request.get('http://localhost:5000/cars/62cde089bd2f9fb1e2a7d772', (err, res, body) => {
+            assert.equal(res.statusCode, 200)
+            done()
+        })
+    })
+})
+
+describe('Login user', () => {
+    it('should login user', (done) => {
+        request.post('http://localhost:5000/auth/login', {
+            json: {
+                name: 'test',
+                email: 'test@test.com',
+                password: 'test123'
+            }
+        }, (err, res, body) => {
+            assert.equal(res.statusCode, 200)
+            done()
+        })
     })
 })
